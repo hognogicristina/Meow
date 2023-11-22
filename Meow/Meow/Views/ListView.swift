773e3c866633641760aba5cf406146dc44c7ac49
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2023-11-22 22:24:41 +0200

    added a search bar for breed

--- Meow/Meow/Views/ListView.swift
+++ Meow/Meow/Views/ListView.swift
@@ -2,33 +2,37 @@ import SwiftUI
 
 struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
-    
+
     @State var selectedCat: EditCat?
-    
+    @State private var searchText = ""
+
+    var filteredCats: [Cat] {
+        if searchText.isEmpty {
+            return listViewModel.cats
+        } else {
+            return listViewModel.cats.filter { $0.breed.lowercased().contains(searchText.lowercased()) }
+        }
+    }
+
     var body: some View {
         VStack {
             Text("Please help our app by updating information about cats if needed.")
                 .font(.system(size: 13))
                 .multilineTextAlignment(.center)
                 .foregroundColor(.blue)
-            
-            if listViewModel.cats.isEmpty {
+
+            SearchBar(searchText: $searchText)
+
+            if filteredCats.isEmpty {
                 Spacer()
                 Text("No cats available for adoption")
                     .font(.system(size: 15))
                     .foregroundColor(.gray)
                 Spacer()
-                
-                .navigationTitle("Cats for adoption")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarItems(
-                    trailing: NavigationLink(destination: AddView()) {
-                        Image(systemName: "plus.circle")
-                    })
             } else {
                 List {
-                    ForEach(listViewModel.cats.indices, id: \.self) { index in
-                        let cat = listViewModel.cats[index]
+                    ForEach(filteredCats.indices, id: \.self) { index in
+                        let cat = filteredCats[index]
                         NavigationLink(destination: CatDetailsView(cat: cat)) {
                             HStack {
                                 ListRowView(cat: cat)
@@ -45,16 +49,18 @@ struct ListView: View {
                 .sheet(item: $selectedCat) { editCat in
                     EditView(cat: editCat.cat)
                 }
-                .navigationTitle("Cats for adoption")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarItems(
-                    trailing: NavigationLink(destination: AddView()) {
-                        Image(systemName: "plus.circle")
-                    })
             }
         }
+        .navigationTitle("Cats for adoption")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarItems(
+            trailing: NavigationLink(destination: AddView()) {
+                Image(systemName: "plus.circle")
+            }
+        )
     }
 }
+
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
